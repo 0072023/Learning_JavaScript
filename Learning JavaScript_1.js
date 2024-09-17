5de9b11fe5123aa4fd743b2893aebb5afcdcc984
0072023 <131215606+0072023@users.noreply.github.com>
2024-09-17 17:31:25 -0300

    Add files via upload

---
+++ Learning JavaScript_1.js
@@ -0,0 +1,6 @@
+console.log('Hello!')
+document.getElementById("myJAVA") .textContent = 'Hello!';
+document.getElementById("myP") .textContent = 'Escrevendo meu primeiro paragrafo!'
+window.alert('Isso e um alerta');
+window.alert('clique em ok para continuar')
+window.alert('confirme mais uma vez')
